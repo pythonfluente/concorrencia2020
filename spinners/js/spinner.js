3959d6d7a6b33c94fc5e398e35e22f971f756117
Lucas Costa <lucasrcosta@gmail.com>
2020-04-20 23:23:16 -0700

    improved Node.js spinner example

--- spinners/js/spinner.js
+++ spinners/js/spinner.js
@@ -1,24 +1,37 @@
 function timer(seconds) {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve('resolved');
-      }, seconds * 1000);
-    });
-  }
-
-const CR = '\r';
-
-async function spin(msg, computation) {
-    out = process.stdout;
-    for (const char of '⠇⠋⠙⠸⠴⠦') {
-        await timer(.1);
-        status = CR + char + ' ' + msg;
-        out.write(status);
-    }
-    blanks = ' '.repeat(status.length);
-    out.write(CR + blanks + CR);
+    return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
 
+function spin(msg) {
+    const out = process.stdout;
+    const CR = '\r';
+    let status;
+
+    function *genStatuses() {
+        while (true) {
+            for (const char of '⠇⠋⠙⠸⠴⠦') {
+                status = CR + char + ' ' + msg;
+                yield status;
+            }
+        }
+    }
+
+    const statuses = genStatuses()
+    new Promise(async () => {
+        await timer(0.1);
+        for (const status of statuses) {
+            out.write(status);
+            await timer(0.1);
+        }
+    })
+
+    const cancel = () => {
+        statuses.return()
+        const blanks = ' '.repeat(status.length);
+        out.write(CR + blanks + CR);
+    };
+    return { cancel };
+}
 
 async function slowFunction() {
     await timer(3);
@@ -26,15 +39,15 @@ async function slowFunction() {
 }
 
 async function supervisor() {
-    const computation = new Promise((resolve, reject) => {});
-    const spinner = spin('thinking!', computation);
+    const spinner = spin('thinking!');
     console.log('spinner object:', spinner);
     const result = await slowFunction();
+    spinner.cancel();
     return result;
 }
 
 async function main() {
-    result = await supervisor();
+    const result = await supervisor();
     console.log('Answer:', result);
 }
 
